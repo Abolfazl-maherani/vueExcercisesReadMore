df05ae86a123aad8de855aa8fc04b8182b7e3248
Abolfazl Maherani <mahrani.prj@gmail.com>
2022-02-17 00:53:57 +0330

    First Commit: the first commit for add files

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import readMore from './components/ReadMore.vue'
+let lengthText = 100;
+const textCard = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
+  massa ipsum. Maecenas mattis congue metus vel fringilla. Praesent id arcu posuere, cursus metus et,
+  venenatis risus. Maecenas sed nunc volutpat, tempus mi sed, tincidunt velit. In malesuada interdum bibendum.
+  Maecenas sed porttitor nulla, et feugiat libero. Vivamus hendrerit enim sed sodales lobortis. Ut egestas vehicula
+  condimentum. Fusce varius libero vel diam bibendum,
+  vel porttitor arcu pharetra. Donec vestibulum venenatis arcu, id luctus eros lacinia vitae. Maecenas vitae sapien ipsum.`
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png"/>
+  <readMore :lengthText="lengthText" text-title="app card" :text-card="textCard"/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ReadMore.vue
@@ -0,0 +1,65 @@
+<script setup>
+import {onMounted,ref,useAttrs} from 'vue';
+import * as v from 'vue';
+let props = defineProps({
+  textTitle: {
+    type: String,
+    required: true
+  },
+  textCard: {
+    required: true,
+    type: String
+  },
+  textSeperate: {
+    default: "...",
+    type: String
+  },
+  lengthText: {
+    type: Number,
+    required: true
+  }
+})
+var text = ref(props.textCard);
+
+let isAbbreviation = true;
+
+function changeString() {
+  if (!isAbbreviation) {
+    text.value = props.textCard;
+  } else {
+    let abbreviation = props.textCard.slice(0, props.lengthText).trim();
+    abbreviation += props.textSeperate;
+    text.value = abbreviation;
+  }
+  isAbbreviation = !isAbbreviation;
+}
+
+// changeString();
+
+</script>
+
+<template>
+  <div class="row">
+    <div class="col-3"></div>
+    <div class="col-6">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">{{ textTitle }}</h5>
+          <p class="card-text ">{{ text }}</p>
+          <a href="#" @click.prevent="changeString" class="btn btn-primary">Read more</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-3"></div>
+  </div>
+</template>
+
+<style lang="scss">
+.card {
+  .card-text {
+    text-align: justify;
+  }
+}
+
+
+</style>
